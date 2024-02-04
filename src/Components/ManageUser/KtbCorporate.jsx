bf0ec7152ecb3473a4984b8110ba3be0e99a8051
Jirasak07 <bee.110243@gmail.com>
2024-02-04 14:14:18 +0700

    Update KtbCorporate.jsx

--- src/Components/ManageUser/KtbCorporate.jsx
+++ src/Components/ManageUser/KtbCorporate.jsx
@@ -1,15 +1,11 @@
 import { Badge, Container } from "@mantine/core";
-import React from "react";
-
 function KtbCorporate() {
   return (
     <>
       <Badge color="var(--primary)" variant="light" size="lg" radius={8}>
-      จัดการข้อมูลการรับเงินผ่านระบบ KTB Corporate Online
+        จัดการข้อมูลการรับเงินผ่านระบบ KTB Corporate Online
       </Badge>
-      <Container bg={"white"} fluid>
-        
-      </Container>
+      <Container bg={"white"} fluid></Container>
     </>
   );
 }
