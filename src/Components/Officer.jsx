e2eb2988c644692748794fc54d5e86bfe44a738e
Jirasak07 <bee.110243@gmail.com>
2024-02-04 13:12:14 +0700

    sd

--- src/Components/Officer.jsx
+++ src/Components/Officer.jsx
@@ -1,4 +1,5 @@
-import { Badge } from "@mantine/core";
+import { Badge, Container, Paper, Text } from "@mantine/core";
+import { MDBDataTableV5 } from "mdbreact";
 import React from "react";
 
 function Officer() {
@@ -7,6 +8,14 @@ function Officer() {
       <Badge color="var(--primary)" variant="light" size="lg" radius={8}>
         ข้อมูลบุคลากร
       </Badge>
+      <Container bg={"white"} fluid>
+        <Paper mt={10}>
+          <Text>รายการบุคลากร </Text>{" "}
+        </Paper>
+        <Paper>
+            <MDBDataTableV5/>
+        </Paper>
+      </Container>
     </>
   );
 }
