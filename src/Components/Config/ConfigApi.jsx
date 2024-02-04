e75deffb24f5c302b9c70cfd1549863b0ee8fd4d
Jirasak07 <bee.110243@gmail.com>
2024-02-04 13:55:43 +0700

    sd

--- src/Components/Config/ConfigApi.jsx
+++ src/Components/Config/ConfigApi.jsx
@@ -1,2 +1,2 @@
-export const API = "https://git.kpru.ac.th/";
-// export const API = "https://mua.kpru.ac.th/";
+export const API = "https://git.kpru.ac.th/FrontEnd_Salary";
+// export const API = "https://mua.kpru.ac.th/FrontEnd_Salary";
--- src/Components/ManageUser/KtbCorporate.jsx
+++ src/Components/ManageUser/KtbCorporate.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { Badge, Container } from "@mantine/core";
+import React from "react";
 
 function KtbCorporate() {
   return (
-    <div>
-      KtbCorporate
-    </div>
-  )
+    <>
+      <Badge color="var(--primary)" variant="light" size="lg" radius={8}>
+      จัดการข้อมูลการรับเงินผ่านระบบ KTB Corporate Online
+      </Badge>
+      <Container bg={"white"} fluid>
+        
+      </Container>
+    </>
+  );
 }
 
-export default KtbCorporate
+export default KtbCorporate;
