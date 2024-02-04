629fd0f250d013144c975bba751eb6814970d86d
Jirasak07 <bee.110243@gmail.com>
2024-02-04 16:48:47 +0700

    szdfsdfsfdsdf

--- src/Components/Publicc/Login.jsx
+++ src/Components/Publicc/Login.jsx
@@ -55,14 +55,14 @@ function Login() {
   };
 
   return (
-    <Container fluid w={"100dvw"} h={"100dvh"} bg={"#e5e5e5"}>
+    <Container fluid w={"100dvw"} h={"100dvh"} bg={{sm:"#e5e5e5",base:"white"}}>
       <LoadingOverlay
         visible={OverLay}
         loaderProps={{ color: "var(--primary)", type: "dots" }}
         overlayProps={{ blur: 1 }}
       />
       <Flex align={"center"} direction={"column"} h={"100%"} justify={"center"}>
-        <Paper shadow="xl" h={550} w={"clamp(350px,95vw,600px)"}>
+        <Paper shadow={{sm:"xl",base:"none"}} h={550} w={"clamp(350px,95vw,600px)"}>
           <Flex pt={15} align={"center"} direction={"column"} justify={"center"}>
             <Image src={Logo} maw={80} />
             <Text c={"var(--primary)"} fz={20} fw={600} ff={"Kanit"}>
