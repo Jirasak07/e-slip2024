dc803613938594e9e0a847a054f98815f9deaf97
Jirasak07 <bee.110243@gmail.com>
2024-02-05 08:41:04 +0700

    Update Salary.jsx

--- src/Components/Salary/Salary.jsx
+++ src/Components/Salary/Salary.jsx
@@ -1,5 +1,24 @@
+import { Badge, Button, Container, Paper, Select } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+
 function Salary() {
-  return <div>sadasd</div>;
+  return (
+    <>
+      <Container p={0} bg={"white"} fluid>
+        <Badge color="var(--primary)" variant="light" size="md" radius={8}>
+          จัดการข้อมูลเงินเดือน
+        </Badge>
+    <Paper>
+      <Select/>
+      <Select/>
+      <Select/>
+      <Button leftSection={<IconSearch/>} >
+        ค้นหา
+      </Button>
+    </Paper>
+      </Container>
+    </>
+  );
 }
 
 export default Salary;
