ceaf5c50b399a2738e7a3bcec8957a0bbaa26109
surachet1414 <surachet1414@gmail.com>
2024-02-07 09:26:01 +0700

    modx

--- src/Components/Salary/ModaladdBudget.jsx
+++ src/Components/Salary/ModaladdBudget.jsx
@@ -3,10 +3,50 @@ import { useForm } from '@mantine/form';
 import { NumberInput, TextInput, Box } from '@mantine/core';
 import { Modal, Button } from '@mantine/core';
 import { Input } from '@mantine/core';
+import { API } from '../Config/ConfigApi';
+
 
 
 export default function ModaladdBudget() {
   const [opened, { open, close }] = useDisclosure(false);
+
+
+  const submitdata =(initialValues)=>{
+        console.log(initialValues.name)
+            const datafrm = new FormData(); //สร้างฟอร์มสำหรับการส่งข้อมูล
+            datafrm.append("OLDID", this.props.OLDID);
+            datafrm.append("details", this.state.auth_tel);
+            datafrm.append("LEV", this.props.LEV);
+      
+            Axios.post(API+"/Finish/addfinishthesisdetails",datafrm,{
+              headers: {
+                  'content-type': 'multipart/form-data'
+              }
+            })
+        
+                              .then(res => {
+                                  console.warn(res);
+                                  console.log("response: ", res)
+                                  if (res.status=== 200) {
+                                  // this.showitem = true;
+                                  // message.success("ลงทะเบียนสำเร็จ")
+                                Swal.fire({
+                                  title: 'บันทึกข้อมูลสำเร็จ',
+                                  icon: 'success',
+                                // showCancelButton: true,
+                                  confirmButtonText: 'ตกลง',
+                                // cancelButtonText: 'No, keep it'
+                                }).then((result) => {
+                                  this.toggle();
+            
+                                })
+      
+                                  }else{
+                                  // window.location.href = '/'
+                                  }
+                                
+                              })
+    }
 
   const form = useForm({
     initialValues: { name: '', email: '', age: 0 },
@@ -25,7 +65,7 @@ export default function ModaladdBudget() {
         {/* Modal content */}
        
         <Box maw={340} mx="auto">
-      <form onSubmit={form.onSubmit(console.log)}>
+      <form onSubmit={form.onSubmit(submitdata)}>
         <TextInput label="Name" placeholder="Name" {...form.getInputProps('name')} />
         <TextInput mt="sm" label="Email" placeholder="Email" {...form.getInputProps('email')} />
         <NumberInput
