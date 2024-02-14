1ad6551e54d051ac95e5623d36a62ed6df762513
surachet1414 <surachet1414@gmail.com>
2024-02-14 10:13:22 +0700

    modx

--- src/Components/menu-salary/Salary/Salary.jsx
+++ src/Components/menu-salary/Salary/Salary.jsx
@@ -1,8 +1,20 @@
-import { Badge, Button, Container, Paper, Select, SimpleGrid } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  Container,
+  Paper,
+  Select,
+  SimpleGrid,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { IconSearch } from "@tabler/icons-react";
 import { MDBDataTableV5 } from "mdbreact";
 import { useEffect, useState } from "react";
+import { API } from "../../Config/ConfigApi";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { Text } from '@mantine/core';
+import SkeletonTable from "../../Publicc-user/SkeletonTable";
 
 function Salary() {
   const column = [
@@ -41,11 +53,7 @@ function Salary() {
       field: "expenses",
       minimal: "lg",
     },
-    {
-      label: "รายจ่าย/นอก",
-      field: "expenses-out",
-      minimal: "lg",
-    },
+   
     {
       label: "ยอดสุทธิ",
       field: "total",
@@ -61,13 +69,147 @@ function Salary() {
     columns: column,
     rows: [],
   });
-  useEffect(() => {}, []);
+  const [LoadTable, setLoadTable] = useState(false);
+  const [DataTypeEmploy, setDataTypeEmploy] = useState([]);
+  const [DataYear, setDataYear] = useState([]);
+
+  const selectmount = [
+    {
+      value: "01",
+      label: "มกราคม",
+    },
+    {
+      value: "02",
+      label: "กุมภาพันธ์",
+    },
+    {
+      value: "03",
+      label: "มีนาคม",
+    },
+    {
+      value: "04",
+      label: "เมษายน",
+    },
+    {
+      value: "05",
+      label: "พฤษภาคม",
+    },
+    {
+      value: "06",
+      label: "มิถุนายน",
+    },
+    {
+      value: "07",
+      label: "กรกฎาคม",
+    },
+    {
+      value: "08",
+      label: "สิงหาคม",
+    },
+    {
+      value: "09",
+      label: "กันยายน",
+    },
+    {
+      value: "10",
+      label: "ตุลาคม",
+    },
+    {
+      value: "11",
+      label: "พฤศจิกายน",
+    },
+    {
+      value: "12",
+      label: "ธันวาคม",
+    },
+  ];
+
+  const FetchTypeEmploy = () => {
+    setLoadTable(true);
+    setTimeout(() => {
+      axios.get(API + "/index/showcustomertype").then((res) => {
+        //    console.log(res.data);
+        const data = res.data;
+        if (data.length !== 0) {
+          setLoadTable(false);
+          const select = data.map((i) => ({
+            value: i.customer_type_id,
+            label: i.customer_type_name,
+          }));
+          setDataTypeEmploy(select);
+        }
+      });
+    }, 400);
+  };
+
+  const FetchYear = () => {
+    // setLoadTable(true);
+    setTimeout(() => {
+      axios.get(API + "/index/showyear").then((res) => {
+        // console.log(res.data);
+        const data = res.data;
+        if (data.length !== 0) {
+          setLoadTable(false);
+          const select = data.map((i) => ({
+            value: i.name_year,
+            label: i.name_year_th,
+          }));
+          setDataYear(select);
+        }
+      });
+    }, 400);
+  };
+
+  const submitdata = (value) => {
+    console.log(value.type_employ);
+    console.log(value.month);
+    console.log(value.year);
+    // const datafrm = new FormData(); //สร้างฟอร์มสำหรับการส่งข้อมูล
+    // datafrm.append("namebudget", initialValues.name);
+    // datafrm.append("levelbudget", initialValues.levelbudget);
+
+    setLoadTable(true);
+
+    axios.get(API+"/index/showrevenueandexpenditure/"+value.type_employ+"/"+value.year+"/"+value.month)
+      .then((res) => {
+        console.warn(res);
+        const data = res.data;
+        if (data.lenth !== 0) {
+          setTableSalary({
+            columns: column,
+            rows: [
+              ...data.map((i, key) => ({
+                no: key + 1,
+                citizen: i.customers_citizent,
+                name: i.customers_pname+i.customers_name+" "+i.customers_lname,
+                type_employ: i.customer_type_name,
+                salary: <Text c="teal.8">{i.history_salary_salary}</Text>,
+                revenue:<Text c="blue">{i.revenue}</Text> ,
+                expenses:<Text c="red.9">{i.expenditure}</Text> ,
+                total: <Text c="dark.9">{i.salary_true}</Text>,
+                manage: <></>
+                // <ModalEditRevenue revenue_id={i.revenue_id} />
+                ,
+              })),
+            ],
+          });
+        }
+        setLoadTable(false);
+      });
+  };
+
+  useEffect(() => {
+    FetchTypeEmploy();
+    FetchYear();
+  }, []);
+
   const formSearch = useForm({
     initialValues: {
       type_employ: "",
       month: "",
       year: "",
     },
+
     validate: {
       type_employ: (v) => (v !== "" ? null : "กรุณาเลือกประเภทบุคลากร"),
       month: (v) => (v !== "" ? null : "กรุณาเลือกเดือน"),
@@ -83,13 +225,26 @@ function Salary() {
         <Paper mt={20}>
           <form
             onSubmit={formSearch.onSubmit((v) => {
-              console.log(v);
+              submitdata(v);
+             // console.log(v);
             })}
           >
             <SimpleGrid cols={4}>
-              <Select {...formSearch.getInputProps("type_employ")} label="ประเภทบุคลากร" />
-              <Select label="เดือน" {...formSearch.getInputProps("month")} />
-              <Select label="ปี" {...formSearch.getInputProps("year")} />
+              <Select
+                data={DataTypeEmploy}
+                {...formSearch.getInputProps("type_employ")}
+                label="ประเภทบุคลากร"
+              />
+              <Select
+                label="เดือน"
+                data={selectmount}
+                {...formSearch.getInputProps("month")}
+              />
+              <Select
+                label="ปี"
+                data={DataYear}
+                {...formSearch.getInputProps("year")}
+              />
               <Button type="submit" mt={33} leftSection={<IconSearch />}>
                 ค้นหา
               </Button>
@@ -97,6 +252,7 @@ function Salary() {
           </form>
         </Paper>
         <Paper pt={20}>
+        {LoadTable ? (<SkeletonTable/>):(
           <MDBDataTableV5
             data={TableSalary}
             responsive
@@ -107,6 +263,8 @@ function Salary() {
             searchBottom={false}
             noRecordsFoundLabel="ไม่พบรายการ"
           />
+        )}
+          
         </Paper>
       </Container>
     </>
