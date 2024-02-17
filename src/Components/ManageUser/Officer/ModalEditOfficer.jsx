e3fe16d274c4c2f6c3d558596cc6ae2f8f39ceb3
Jirasak07 <bee.110243@gmail.com>
2024-02-17 11:54:14 +0700

    asd

--- src/Components/ManageUser/Officer/ModalEditOfficer.jsx
+++ src/Components/ManageUser/Officer/ModalEditOfficer.jsx
@@ -2,29 +2,63 @@ import { Button, Flex, Modal, Select, SimpleGrid, TextInput } from "@mantine/cor
 import { useDisclosure } from "@mantine/hooks";
 import { IconDeviceFloppy, IconEdit } from "@tabler/icons-react";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { API } from "../../Config/ConfigApi";
+import { useForm } from "@mantine/form";
 
 function ModalEditOfficer({ customerid }) {
   const [opened, { open, close }] = useDisclosure(false);
+  const [DataSelectTypeCustomer, setDataSelectTypeCustomer] = useState([]);
+  const formEmploy = useForm({
+    initialValues: {
+      customers_citizent: customerid,
+      fname: "",
+      lname: "",
+      customer_type_id: "",
+      customer_status_id: "",
+    },
+    validate: {},
+  });
+  const FetchTypeCustomer = (params) => {
+    axios.get(API + "/index/showcustomertype").then((res) => {
+      const data = res.data;
+      if (data.length !== 0) {
+        const menu = data.map((i) => ({
+          value: i.customer_type_id,
+          label: i.customer_type_name,
+        }));
+        setDataSelectTypeCustomer(menu);
+      }
+    });
+  };
   const FetchDataEmploy = (params) => {
-    axios.get(API+"/index/showcustomer/"+customerid).then((res)=>{
-      console.log(res.data)
-    })
-  }
-  
+    axios.get(API + "/index/showcustomerdetail/" + customerid).then((res) => {
+      console.log(res.data);
+      const data = res.data;
+      if (data.length !== 0) {
+        const value = data[0];
+        formEmploy.setValues({
+          customer_status_id: value.customers_status,
+          customer_type_id: value.customers_type,
+          fname: value.customers_pname + value.customers_name,
+          lname: value.customers_lname,
+        });
+      }
+    });
+  };
+
   return (
     <>
       <Modal title="แก้ไขข้อมูลพนักงาน" opened={opened} onClose={close}>
         <SimpleGrid>
-          <TextInput disabled label="เลขบัตรประชาชน/รหัสพนักงาน" />
+          <TextInput {...formEmploy.getInputProps("customers_citizent")} disabled label="เลขบัตรประชาชน/รหัสพนักงาน" />
           <SimpleGrid cols={2}>
-            <TextInput readOnly label="ชื่อ" />
-            <TextInput readOnly label="นามสกุล" />
+            <TextInput {...formEmploy.getInputProps("fname")} readOnly label="ชื่อ" />
+            <TextInput {...formEmploy.getInputProps("lname")} readOnly label="นามสกุล" />
           </SimpleGrid>
           <SimpleGrid cols={2}>
-            <Select readOnly disabled label="ประเภท" />
-            <Select allowDeselect={false} label="สถานะ" />
+            <Select data={DataSelectTypeCustomer} {...formEmploy.getInputProps("customer_type_id")} readOnly disabled label="ประเภท" />
+            <Select {...formEmploy.getInputProps("customer_status_id")} allowDeselect={false} label="สถานะ" />
           </SimpleGrid>
         </SimpleGrid>
         <Flex justify={"flex-end"} pt={10} gap={5}>
@@ -36,10 +70,16 @@ function ModalEditOfficer({ customerid }) {
           </Button>
         </Flex>
       </Modal>
-      <Button onClick={()=>{
-        FetchDataEmploy()
-        open()
-      }} leftSection={<IconEdit />} size="xs" color="var(--warning)">
+      <Button
+        onClick={() => {
+          FetchTypeCustomer()
+          FetchDataEmploy();
+          open();
+        }}
+        leftSection={<IconEdit />}
+        size="xs"
+        color="var(--warning)"
+      >
         แก้ไข
       </Button>
     </>
