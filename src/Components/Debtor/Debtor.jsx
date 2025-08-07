4d52d694713817de626b0c247cc3ade19f179a6a
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2025-08-07 11:03:26 +0700

    Update Debtor.jsx

--- src/Components/Debtor/Debtor.jsx
+++ src/Components/Debtor/Debtor.jsx
@@ -1,5 +1,65 @@
+import { Button, Flex, Paper } from "@mantine/core";
+import { IconSettings } from "@tabler/icons-react";
+import { MDBDataTableV5 } from "mdbreact";
+import { useState } from "react";
+
 function Debtor() {
-  return <div>asdasd</div>;
+  const columns = [
+    {
+      label: "ลำดับ",
+      field: "no",
+    },
+    {
+      label: "วัน เดือน ปี",
+      field: "date",
+    },
+    {
+      label: "ชื่อผู้ยืม",
+      field: "name",
+    },
+    {
+      label: "วัตถุประสงค์การยืม",
+      field: "reason",
+    },
+    {
+      label: "จำนวนเงินค้าง",
+      field: "amount",
+    },
+    {
+      label: "ใบยืมที่",
+      field: "billno",
+    },
+    {
+      label: "วันครบกำหนด",
+      field: "enddate",
+    },
+    {
+      label: "หมายเหตุ",
+      field: "note",
+    },
+    {
+      label: "จำนวนเงินค้าง",
+      field: "total",
+    },
+    {
+      label: <IconSettings />,
+      field: "manage",
+    },
+  ];
+  const [Data, setData] = useState({
+    columns: columns,
+    rows: [],
+  });
+  return (
+    <>
+      <Flex px={30} justify={"flex-end"}>
+        <Button>เพิ่มข้อมูลลูกหนี้</Button>
+      </Flex>
+      <Paper p={10} shadow="sm">
+        <MDBDataTableV5 data={Data} />
+      </Paper>
+    </>
+  );
 }
 
 export default Debtor;
