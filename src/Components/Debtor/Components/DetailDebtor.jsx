d1ac64cdbc837cfab6db3ce4d8133da1c1f68b94
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2025-09-09 15:50:23 +0700

    Update DetailDebtor.jsx

--- src/Components/Debtor/Components/DetailDebtor.jsx
+++ src/Components/Debtor/Components/DetailDebtor.jsx
@@ -1,12 +1,48 @@
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, Divider, Flex, Modal, Paper, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { IconEye } from "@tabler/icons-react";
 
 function DetailDebtor({ DEBTOR_ID }) {
+  const [opened, { open, close }] = useDisclosure(false);
+  const OpenDetail = async () => {
+    try {
+      open();
+      // console.log(object)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <ActionIcon   color="var(--primary)" >
+      <ActionIcon
+        onClick={() => {
+          OpenDetail();
+        }}
+        color="var(--primary)"
+      >
         <IconEye />
       </ActionIcon>
+      <Modal title="ข้อมูลลูกหนี้เงินยืมนอกงบประมาณ" opened={opened} onClose={close}>
+        <Paper>
+          <Text fw={600}>ผู้ยืม</Text>
+          <Text>นายจิรศักดิ์ สิงหบุตร</Text>
+        </Paper>
+        <Divider variant="dashed" my={5} size={"sm"} />
+        <Paper>
+          <Text fw={600}>รายละเอียด</Text>
+          <Text>ใบยืมเลขที่ : 12</Text>
+          <Text>วันที่ยืม : 11/01/2025 </Text>
+          <Text>วันครบกำหนด : 20/01/2025</Text>
+          <Text>จำนวนเงินค้าง : 32,210.00</Text>
+          <Text>จำนวนเงินค้างคงเหลือ : 32,210.00</Text>
+         <Divider variant="dashed" my={5} size={"sm"} />
+          <Flex>
+            <Text mx={"auto"}>วัตถุประสงค์การยืม</Text>
+          </Flex>
+          <Text>วัตถุประสงค์การยืม</Text>
+        </Paper>
+      </Modal>
     </>
   );
 }
