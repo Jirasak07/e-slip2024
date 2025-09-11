23a4e5001f8ec6452e5a63fc9781711b471fbe7b
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2025-09-11 11:56:38 +0700

    Update EditDebtor.jsx

--- src/Components/Debtor/Components/EditDebtor.jsx
+++ src/Components/Debtor/Components/EditDebtor.jsx
@@ -1,17 +1,24 @@
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, Divider, Modal, Paper } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { IconEdit } from "@tabler/icons-react";
 
 function EditDebtor({ FN, DEBTOR_ID }) {
+  const [opened, { open, close }] = useDisclosure(false);
   return (
     <>
       <ActionIcon
         color="orange.5"
         onClick={() => {
+          open();
           console.log("ee");
         }}
       >
         <IconEdit />
       </ActionIcon>
+      <Modal title="แก้ไขรายละเอียดลูกหนี้นอกงบประมาณ" opened={opened} onClose={close}>
+        <Paper p={10}>sahgdjhsagdjhas</Paper>
+        <Divider my={5} variant="dashed" size={"sm"} />
+      </Modal>
     </>
   );
 }
